01f98a01dedbe451393b333f75ae4256b5ceb8da
godseykj <kjgodsey2@gmail.com>
2022-11-17 21:12:28 -0500

    rough draft 1 of the graph

---
+++ BooksSummary2022.R
@@ -0,0 +1,13 @@
+library(ggplot2)
+
+books <- read.csv("Books2022.csv")
+books$num <- seq(1:length(books$Title))
+
+#11/17/22
+plot1 <- ggplot(books, aes(x = num, y = Words, fill = Genre)) +
+  geom_bar(stat="identity") +
+  geom_text(aes(label = Title, size = Words), color = "black", position = position_stack(vjust = 0.5), angle = 90) +
+  scale_radius(range = c(3.5, 9)) +
+  labs(x = "") +
+  theme_classic()
+plot1
